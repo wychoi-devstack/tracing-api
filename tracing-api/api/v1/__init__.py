9ea4326d19949d09972d4a87d762a42e057cdfa7
wychoi-devstack <wychoi@devstack.co.kr>
2024-08-20 07:08:26 +0000

    init: initial setting

---
+++ tracing-api/api/v1/__init__.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter
+#from . import ''
+
+api_router = APIRouter()
+#api_router.include_router(''.router, tags=[""])
---
+++ tracing-api/config/__init__.py
@@ -0,0 +1,17 @@
+from oslo_config import cfg
+from . import default,cors
+
+CONF = cfg.CONF
+
+conf_modules = [default,
+        cors
+]
+
+def configure(conf=None, config_file_path='/etc/tracing-api/tracing-api.conf'):
+    if conf is None:
+        conf = CONF
+
+    for module in conf_modules:
+        module.register_opts(conf)
+
+    CONF(['--config-file='+config_file_path], project='tracing-api')
